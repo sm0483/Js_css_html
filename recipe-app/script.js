c71f485b69ff2338254d143a69d9d9decf568ae5
“sreerag” <msratofficial@gmail.com>
2022-05-13 23:17:09 +0530

    feat: show info when i button clicked

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -4,6 +4,7 @@ const searchTermIn=document.querySelector('.search-text')
 const searchBtnIn=document.querySelector('.search-button');
 const favButtonIn=document.querySelectorAll('.fav-button');
 const favMealListIn=document.querySelector('.fav-meals');
+const detailIn=document.querySelector('#id-pop-up');
 
 //TODO 
 //create a liked list and pop about it
@@ -48,7 +49,7 @@ let changeBtnColor=  (id)=>{
     //event listner for general button in html page
 
     let btn=document.querySelector(`#id${id}`);
-    btn.addEventListener('click',(res)=>{
+    btn.addEventListener('click',()=>{
         //let likeMealId=res.target.id.substring(2);
         if(!isPresentInLikedMeal(id)){
             likedMealData=getMealbyId(id)
@@ -63,9 +64,9 @@ let changeBtnColor=  (id)=>{
                 })
         }
         else if(isPresentInLikedMeal(id)){
-        console.log(isPresentInLikedMeal(id));
+            console.log(isPresentInLikedMeal(id));
             if(btn!==undefined){
-            btn.classList.remove('active');
+                btn.classList.remove('active');
             }
             rmLikedMeal(id);
             addToFavorite();
@@ -92,6 +93,9 @@ let addDataToMealBody=(mealData,random=false)=>{
                     <button class="fav-button", id="id${mealData.idMeal}">
                         <i class="fas fa-heart",id="heart${mealData.idMeal}" ></i>
                     </button>
+                    <button class='info' id='info${mealData.idMeal}'>
+                        <i class="fa-solid fa-info"></i>
+                    </button>
                 </div>
 
     `;
@@ -99,8 +103,7 @@ let addDataToMealBody=(mealData,random=false)=>{
 
     //save all button to list
     changeBtnColor(mealData.idMeal);
-
-
+    popContainer(mealData.idMeal);
 
 }
 
@@ -115,12 +118,12 @@ let removeFav =(id)=>{
         console.log(err);
 
     }
-    btn.addEventListener('click', (res)=>{
+    btn.addEventListener('click', ()=>{
         //console.log(likedMeal);
         rmLikedMeal(id);
         addToFavorite();
         try{
-        btnInMain.classList.remove('active');
+            btnInMain.classList.remove('active');
         }
         catch(err){
             console.log(err);
@@ -129,6 +132,7 @@ let removeFav =(id)=>{
 
     })
 }
+
 
 let addToFavorite= ()=>{
     //store meal data
@@ -146,9 +150,11 @@ let addToFavorite= ()=>{
         li.innerHTML=`
                     <img src="${mealData.strMealThumb}" alt=" ">
                     <span>${mealData.strMeal}</span>
+                    <span><button class="rm-button"id="info${mealData.idMeal}">&lt</button></span>
                     <span><button class="rm-button"id="rm${mealData.idMeal}">&times</button></span>
         `
         favMealListIn.appendChild(li);
+        popContainer(mealData.idMeal);
         //rm button
         removeFav(mealData.idMeal);
 
@@ -157,6 +163,54 @@ let addToFavorite= ()=>{
 
 }
 
+let removeInfo=()=>{
+    detailIn.innerHTML='';
+
+}
+
+let createPop=(meal)=>{
+    detailIn.innerHTML=  
+    `<div class='info-body'>
+            <div class='info-head'>
+                <h1>${meal.strMeal}<h1>
+                <button class="remove-detail">&times</button>
+                <img src="${meal.strMealThumb}" alt=" ">
+                </div>
+                <div class='info-body'>
+                <h3>Orgin ${meal.strArea}<h3>
+                <p>
+                    ${meal.strInstructions}
+                </p>        
+             </div>
+        </div>`;
+    let btn=document.querySelector('.remove-detail');
+    btn.addEventListener('click', ()=>{
+        removeInfo();
+    })
+
+
+
+}
+
+let popContainer= (id,fromFav=false)=>{
+    //clear already inserted
+    removeInfo();
+    //test meal id
+    //add data
+    let btn=document.querySelector(`#info${id}`);
+    btn.addEventListener('click', ()=>{
+        detailIn.classList.remove('deactive');
+        getMealbyId(id)
+            .then((meal)=>{
+                createPop(meal[0]);
+            })
+            .catch((err)=>{
+                console.log(err);
+
+            })
+    })
+
+}
 
 
 
